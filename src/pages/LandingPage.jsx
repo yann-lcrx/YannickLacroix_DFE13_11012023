c5aeef593d935c8e315a75d621fa3fa077547de7
Yannick Lacroix <yannick.lacroix@hubvisory.com>
2023-03-05 21:30:10 +0100

    feat: remove unused files

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
 
-function LandingPage({}) {
+function LandingPage() {
   return (
     <>
       <main>
