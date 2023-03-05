e7b403595ce5268d2fb4277f0f22e53a046e7371
Yannick Lacroix <yannick.lacroix@hubvisory.com>
2023-03-05 21:34:13 +0100

    refactor: remove react fragments

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,32 +18,30 @@ function Login() {
   };
 
   return (
-    <>
-      <main className="main bg-dark">
-        <section className="sign-in-content">
-          <i className="fa fa-user-circle sign-in-icon"></i>
-          <h1>Sign In</h1>
-          <form onSubmit={submitForm}>
-            <div className="input-wrapper">
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" name="username" required />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" name="password" required />
-            </div>
-            {authError && <p className="error-text">{authError}</p>}
-            <div className="input-remember">
-              <input type="checkbox" id="remember-me" />
-              <label htmlFor="remember-me">Remember me</label>
-            </div>
-            <button type="submit" className="sign-in-button">
-              Sign In
-            </button>
-          </form>
-        </section>
-      </main>
-    </>
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form onSubmit={submitForm}>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          {authError && <p className="error-text">{authError}</p>}
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <button type="submit" className="sign-in-button">
+            Sign In
+          </button>
+        </form>
+      </section>
+    </main>
   );
 }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -35,116 +35,101 @@ function Profile() {
   }, []);
 
   return (
-    <>
-      <main className="main bg-dark">
-        {profileError ? (
-          <p className="error-text">{profileError}</p>
-        ) : (
-          <>
-            {" "}
-            <div className="header">
-              <h1>
-                Welcome back
-                {!isEditMode && (
-                  <>
-                    <br />
-                    {username}!
-                  </>
-                )}
-              </h1>
-              {nameChangeError && (
-                <p className="error-text">{nameChangeError}</p>
-              )}
-              {isEditMode ? (
+    <main className="main bg-dark">
+      {profileError ? (
+        <p className="error-text">{profileError}</p>
+      ) : (
+        <>
+          {" "}
+          <div className="header">
+            <h1>
+              Welcome back
+              {!isEditMode && (
                 <>
-                  <form onSubmit={handleChangeName} className="edit-form">
-                    <div className="edit-section">
-                      <input
-                        required
-                        min={2}
-                        placeholder={firstName}
-                        name="firstName"
-                        aria-label="first name"
-                        className="edit-field"
-                      />
-                      <input
-                        required
-                        min={2}
-                        placeholder={lastName}
-                        name="lastName"
-                        aria-label="last name"
-                        className="edit-field"
-                      />
-                    </div>
-                    <div className="edit-section">
-                      <button
-                        type="submit"
-                        className="edit-button edit-button-control"
-                      >
-                        Save
-                      </button>
-                      <button
-                        type="button"
-                        className="edit-button edit-button-control edit-button-cancel"
-                        onClick={() => setEditMode(false)}
-                      >
-                        Cancel
-                      </button>
-                    </div>
-                  </form>
+                  <br />
+                  {username}!
                 </>
-              ) : (
-                <button
-                  className="edit-button"
-                  onClick={() => setEditMode(true)}
-                >
-                  Edit Name
-                </button>
               )}
+            </h1>
+            {nameChangeError && <p className="error-text">{nameChangeError}</p>}
+            {isEditMode ? (
+              <>
+                <form onSubmit={handleChangeName} className="edit-form">
+                  <div className="edit-section">
+                    <input
+                      required
+                      min={2}
+                      placeholder={firstName}
+                      name="firstName"
+                      aria-label="first name"
+                      className="edit-field"
+                    />
+                    <input
+                      required
+                      min={2}
+                      placeholder={lastName}
+                      name="lastName"
+                      aria-label="last name"
+                      className="edit-field"
+                    />
+                  </div>
+                  <div className="edit-section">
+                    <button
+                      type="submit"
+                      className="edit-button edit-button-control"
+                    >
+                      Save
+                    </button>
+                    <button
+                      type="button"
+                      className="edit-button edit-button-control edit-button-cancel"
+                      onClick={() => setEditMode(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              </>
+            ) : (
+              <button className="edit-button" onClick={() => setEditMode(true)}>
+                Edit Name
+              </button>
+            )}
+          </div>
+          <h2 className="sr-only">Accounts</h2>
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+              <p className="account-amount">$2,082.79</p>
+              <p className="account-amount-description">Available Balance</p>
             </div>
-            <h2 className="sr-only">Accounts</h2>
-            <section className="account">
-              <div className="account-content-wrapper">
-                <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                <p className="account-amount">$2,082.79</p>
-                <p className="account-amount-description">Available Balance</p>
-              </div>
-              <div className="account-content-wrapper cta">
-                <button className="transaction-button">
-                  View transactions
-                </button>
-              </div>
-            </section>
-            <section className="account">
-              <div className="account-content-wrapper">
-                <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                <p className="account-amount">$10,928.42</p>
-                <p className="account-amount-description">Available Balance</p>
-              </div>
-              <div className="account-content-wrapper cta">
-                <button className="transaction-button">
-                  View transactions
-                </button>
-              </div>
-            </section>
-            <section className="account">
-              <div className="account-content-wrapper">
-                <h3 className="account-title">
-                  Argent Bank Credit Card (x8349)
-                </h3>
-                <p className="account-amount">$184.30</p>
-                <p className="account-amount-description">Current Balance</p>
-              </div>
-              <div className="account-content-wrapper cta">
-                <button className="transaction-button">
-                  View transactions
-                </button>
-              </div>
-            </section>
-          </>
-        )}
-      </main>
-    </>
+            <div className="account-content-wrapper cta">
+              <button className="transaction-button">View transactions</button>
+            </div>
+          </section>
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+              <p className="account-amount">$10,928.42</p>
+              <p className="account-amount-description">Available Balance</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button className="transaction-button">View transactions</button>
+            </div>
+          </section>
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+              <p className="account-amount">$184.30</p>
+              <p className="account-amount-description">Current Balance</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button className="transaction-button">View transactions</button>
+            </div>
+          </section>
+        </>
+      )}
+    </main>
   );
 }
 
